a166f0a4c67a0f0dd0b178b6d08dc249fcaa2db9
61010817 <61010817@kmitl.ac.th>
2020-11-22 02:56:33 +0700

    add about_us sceen

--- screen/About_usScreen.js
+++ screen/About_usScreen.js
@@ -4,12 +4,15 @@ import {
     View, Text, TextInput,
     StyleSheet, Button,
     TouchableWithoutFeedback,
-    TouchableOpacity,
+    TouchableOpacity,block,
     Keyboard
 } from 'react-native';
-import{Entypo } from'@expo/vector-icons';
-import{FontAwesome}from'@expo/vector-icons';
-import{Ionicons}from'@expo/vector-icons';
+
+import{
+    Entypo,FontAwesome,
+    Ionicons,MaterialIcons
+}from'@expo/vector-icons';
+
 import Block from '../components/Block';
 import Colors from '../constants/color';
 import Input from '../components/Input';
@@ -20,50 +23,25 @@ import bgImage from '../assets/About_us.png';
 const About_usScreen = (props) => {
     // console.log(props);
     return (
-        <ImageBackground source={bgImage} style={styles.screen}>   
-                
-               
+        <ImageBackground source={bgImage} style={styles.screen}>
+            
                 <View>
-                < Entypo
-                 raised
-                 name='facebook-with-circle' 
-                 size={30} 
-                 color= '#4658fc'
-                 styles={{marginTop:60}}/>
-                 < Entypo
-                 raised
-                 name='mail' 
-                 size={30} 
-                 color= '#F7EC90'/>
-                 < FontAwesome
-                 raised
-                 name='home' 
-                 size={30} 
-                 color= '#8B5353'/>
-                 < Ionicons
-                 raised
-                 name='ios-call' 
-                 size={30} 
-                 color= '#7BCDCD'/>
-
-                    <Buttons style={styles.buttonAbout_us}>
-                        <Text style={styles.Text}>S1G5</Text>
-                        <Text style={styles.Text}>S1G5@hotmail.com</Text>
-                        <Text style={styles.Text}>KMITL,Lardkrabang</Text>
-                        <Text style={styles.Text}>012-3456789</Text>
-                    </Buttons>
-                    
-                    </View>
-                   
+                <MaterialIcons name ='navigate-before' size={60} color="white" style={{marginTop:40,marginLeft:-36 }}/>
+                    <Block style={styles.blockAbout_us}>
+                        <Text style={styles.Text}></Text>
+                        <Text style={styles.Text}></Text>
+                        <Text style={styles.Text}><Entypo name ='facebook-with-circle' size={30} color= '#4658fc'/>   S1G5</Text>
+                        <Text style={styles.Text}><Entypo name ='mail'size={30} color= '#F7EC90'/>  S1G5@hotmail.com</Text>
+                        <Text style={styles.Text}><FontAwesome name ='home'size={30} color= '#8B5353'/>   KMITL,Lardkrabang</Text>
+                        <Text style={styles.Text}><Ionicons name ='ios-call'size={30}  color= '#7BCDCD'/>   012-3456789</Text>
+                    </Block>
+                </View>
         </ImageBackground >
     );
 };
 
 const styles = StyleSheet.create({
     backgroundContainer: {
-        flex: 1,
-        height: null,
-        width: null,
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
@@ -79,16 +57,16 @@ const styles = StyleSheet.create({
     //     padding: 30,
     //     paddingHorizontal: 15,
     // },
-    buttonAbout_us: {
-        width: 300,
-        height: 200,
-        marginTop: 200,
+    blockAbout_us: {
+        width: 350,
+        height: 300,
+        marginTop: 160,
     },
     Text: {
         color: 'black',
         fontSize: 26,
-        textAlign: 'left',
-        fontFamily:'baloo-bhaina'
+        marginLeft:30,
+        fontFamily:'baloo-bhaina',
     },
     
 });
